d2eb47e5abba0a2d9fc27b655f2ab7bf320f6748
Shen Weizhong <swzyocowboy@icloud.com>
2014-01-12 18:16:16 +0800

    initial commit for transaction detail page

---
+++ cdn/ajax/libs/js/investment_site/app/account@3.main.js
@@ -0,0 +1,93 @@
+/**
+ * Statement: ...//TODO: Write statement.
+ * 
+ * Describe: The business logic bootstrap script file for the transaction detail page (../../../../../../www/account@3.html).    ...//TODO: Check description.
+ * 
+ * Further Changes, Comments: ...//TODO: Give a further changes and comments link.
+ * 
+ * Javascript Design Pattern (Code Management):    ...//TODO: Cehck design pattern.
+ *     
+ *     Namespacing Patterns, Immediately-invoked Function Expressions (IIFE)s
+ *     
+ *     Modules Patterns, Revealing Module Pattern
+ *     
+ *     Modules Patterns, AMD modules
+ *     
+ * Docs: ...//TODO: Give a link about project documents.
+ * 
+ * Original Author: Shen Weizhong ( Tony Stark ).
+ * 
+ * Thanks: ...//TODO: If there are some contributors, just mark them.
+ * 
+ * Version: 0.1.0-alpha
+ * 
+ * Creation Date: 2014.01.09 17:41 ( Tony ).
+ * 
+ * Last Update: 2014.01.10 12:43 ( Tony ).    ...//TODO: Update the 'Last Update'.
+ * 
+ * Music ( Custom ): Countdown (feat. Makj).mp3    ...//TODO: If you are listenning some music, just write the name of songs.
+ * 
+ * License: ...//TODO: Give a license.
+ * 
+ * Copyright: ...//TODO: Give a copyright.
+ */
+
+(function (require, console) {
+	
+	var fn, _AMD;
+	
+	fn = function (require) {
+		
+		var modernizr, SJ, basicLogic/*, plusLogic*/, modifyTitle/*, deepLink*/, srl, cont;
+		
+		modernizr = require('modernizr');
+		
+		SJ = require('jquery');
+		
+		basicLogic = require('logic/account@3.logic.min');
+		
+		// plusLogic = require('logic/account@3.logic.plus');
+		
+		modifyTitle = require('titleModify');
+		
+		// deepLink = require('deepLink');
+		
+		srl = require('srl');
+		
+		cont = require('cont');
+		
+		SJ(function ($) {
+			
+			// TODO: Import basic business logic script here.
+			
+			$('html').studioScroll({zindex: 50, cursorborder: 0, cursorborderradius: 0});
+			
+			modifyTitle();
+			
+			//deepLink.init();
+			
+		});
+		
+	};
+	
+	_AMD = (function (_register, _module) {
+		
+		var hasDefine, registryProfile;
+		
+		hasDefine = typeof define === "function" && define.amd;
+		
+		registryProfile = function () {
+			
+			hasDefine ? define(_module) : console.error('Sorry! There is no "define" object.');
+			
+		};
+		
+		return {
+			
+			init: registryProfile
+			
+		};
+		
+	}(_AMD || {}, fn)).init();
+	
+} (require, (typeof console !== 'undefined' ? console : undefined)));
