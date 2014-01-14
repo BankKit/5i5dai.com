ad26f52db88b828b671fcb8036a83b0d4d9fb5e6
Shen Weizhong <swzyocowboy@icloud.com>
2014-01-14 19:09:07 +0800

    initial commit for " find password " page

---
+++ cdn/ajax/libs/js/investment_site/app/find.password.main.js
@@ -0,0 +1,91 @@
+/**
+ * Statement: ...//TODO: Write statement.
+ * 
+ * Describe: The business logic bootstrap script file for the " find password " page (../../../../../../www/find.password.html).    ...//TODO: Check description.
+ * 
+ * Further Changes, Comments: ...//TODO: Give a further changes and comments link.
+ * 
+ * Javascript Design Pattern (Code Management):    ...//TODO: Cehck design pattern.
+ *     
+ *     Namespacing Patterns, Immediately-invoked Function Expressions (IIFE)s
+ *     
+ *     Modules Patterns, Revealing Module Pattern
+ *     
+ *     Modules Patterns, AMD modules
+ *     
+ * Docs: ...//TODO: Give a link about project documents.
+ * 
+ * Original Author: Shen Weizhong ( Tony Stark ).
+ * 
+ * Thanks: ...//TODO: If there are some contributors, just mark them.
+ * 
+ * Version: 0.1.0-alpha
+ * 
+ * Creation Date: 2014.01.14 17:20 ( Tony ).
+ * 
+ * Last Update: 2014.01.14 19:03 ( Tony ).    ...//TODO: Update the 'Last Update'.
+ * 
+ * Music ( Custom ): Countdown (feat. Makj).mp3    ...//TODO: If you are listenning some music, just write the name of songs.
+ * 
+ * License: ...//TODO: Give a license.
+ * 
+ * Copyright: ...//TODO: Give a copyright.
+ */
+
+(function (require, console) {
+	
+	var fn, _AMD;
+	
+	fn = function (require) {
+		
+		var SJ, basicLogic/*, plusLogic*/, modifyTitle/*, deepLink*/, srl, cont;
+		
+		SJ = require('jquery');
+		
+		basicLogic = require('logic/find.password.logic.min');
+		
+		/*plusLogic = require('logic/find.password.logic.plus');*/
+		
+		modifyTitle = require('titleModify');
+		
+		// deepLink = require('deepLink');
+
+		srl = require('srl');
+
+		cont = require('cont');
+		
+		SJ(function ($) {
+			
+			// TODO: Import basic business logic script here.
+			
+			$('html').studioScroll({zindex: 50, cursorborder: 0, cursorborderradius: 0});
+			
+			modifyTitle();
+			
+			//deepLink.init();
+			
+		});
+		
+	};
+	
+	_AMD = (function (_register, _module) {
+		
+		var hasDefine, registryProfile;
+		
+		hasDefine = typeof define === "function" && define.amd;
+		
+		registryProfile = function () {
+			
+			hasDefine ? define(_module) : console.error('Sorry! There is no "define" object.');
+			
+		};
+		
+		return {
+			
+			init: registryProfile
+			
+		};
+		
+	}(_AMD || {}, fn)).init();
+	
+} (require, (typeof console !== 'undefined' ? console : undefined)));
